e44f66fd0ceb5161779dd9d68f12084058c85fd5
jg312551 <48728410+jg312551@users.noreply.github.com>
2019-10-10 09:55:09 -0500

    new

---
+++ src/HangMan.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class HangMan {
+    public static void main(String[] args){
+        String word = "word";
+        Scanner koal=new Scanner(System.in);
+        String inputStr;
+
+        System.out.println("enter a letter");
+        inputStr=koal.next();
+        char input = inputStr.charAt(0);
+
+        for(int i=0; i<word.length();i++) {
+
+
+
+
+
+
+     }
+    }
+}
