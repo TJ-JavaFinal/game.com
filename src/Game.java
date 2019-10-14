00ce9e5ee35f76636a8b3dee14bfafe54a1bec91
Thomas403 <48694574+Thomas403@users.noreply.github.com>
2019-10-14 09:49:35 -0500

    update change

---
+++ src/Game.java
@@ -0,0 +1,17 @@
+public class Game {
+
+    public static void main(String[] args) {
+
+        System.out.println("                         ");
+        System.out.println("Welcome to The Puzzle game");
+        System.out.println("                         ");
+        System.out.println("instructions: ");
+        System.out.println("In this game you will start by having to choose between three different doors");
+        System.out.println("The first doors will then have a randomly generated puzzle you have to figure out");
+        System.out.println("The puzzle could be entering a password,entering a code, or diffusing a bomb if you fail the bomb defusal it will make you restart");
+        System.out.println("After you get through the first door you chose you will then be given a choice between two more doors");
+        System.out.println("Each of these doors will have a randomly generated puzzle or an automatic restart");
+        System.out.println("If you finish the last puzzle you can then leave through the last door which is the Exit then you have completed The Puzzle game");
+
+    }
+}
