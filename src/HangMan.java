63ba8f683503d022f095f9e8d3683114bb0a4d9e
jg312551 <48728410+jg312551@users.noreply.github.com>
2019-10-15 09:13:09 -0500

    update

--- src/HangMan.java
+++ src/HangMan.java
@@ -1,22 +1,58 @@
 import java.util.Scanner;
 
 public class HangMan {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String word = "word";
-        Scanner koal=new Scanner(System.in);
+        Scanner key = new Scanner(System.in);
         String inputStr;
+        boolean exit = true;
+        int chances = 5;
+        int  wrongCount=0;
+        char[] rightLetter= new char[4];
+        int winCount;
 
-        System.out.println("enter a letter");
-        inputStr=koal.next();
-        char input = inputStr.charAt(0);
-
-        for(int i=0; i<word.length();i++) {
-
-
+        while (exit) {
+            winCount=0;
+            System.out.println("enter a letter>>>>");
+            inputStr = key.next();
+            char input = inputStr.charAt(0);
 
 
+            for (int i = 0; i < 4; i++) {
 
 
-     }
+                if (word.charAt(i) == input) {
+                    rightLetter[i]= input;
+                    System.out.println("you got it right");
+                }
+
+
+                else
+                    wrongCount += 1;
+
+
+                 if (wrongCount == 4) {
+                    chances--;
+                    wrongCount=0;
+                    System.out.println(" you lost a chance. " + chances + " chances left");
+
+                }
+               else if(rightLetter[i]==word.charAt(i))
+                 winCount += 1;
+            }
+
+                if (chances == 0) {
+                    System.out.println("you lose");
+                    exit = false; }
+
+            else if(winCount==4){
+                System.out.println("you win!");
+                exit=false;
+                }
+        }
+
     }
 }
+
+
+
