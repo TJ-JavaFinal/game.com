5c39df33d1223f0d826d4e4676fbfc56225ae91d
Thomas403 <48694574+Thomas403@users.noreply.github.com>
2019-10-15 09:51:40 -0500

    update change

--- src/Doors.java
+++ src/Doors.java
@@ -5,14 +5,25 @@ public class Doors {
     public static void main(String[] args) {
 
         int choice;
-        boolean text = true;
-        while (text){
+        boolean doorsActive = true;
+        while (doorsActive) {
             Scanner input = new Scanner(System.in);
             System.out.println("Choose a door! \n 1- for Door 1 " +
-                    "\n 1- for Door 2 \n 3 - for Door 3 \n choice >>>>");
+                    "\n 2- for Door 2 \n 3 - for Door 3 \n choice >>>>");
             choice = input.nextInt();
+            System.out.println("You choose Door Number " + choice + " now you will have to complete a puzzle in this room to be able go on to the next choices of rooms.");
+
 
         }
+    }
+
+    public static void secondRoomDoors() {
+        int choice;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Choose a door! \n 1- for Door 1 " +
+                "\n 2- for Door 2 \n choice >>>>");
+        choice = input.nextInt();
+
 
 
     }
