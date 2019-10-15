da6c23a4739f0dc32c1040c2729280d65e3bdefe
Thomas403 <48694574+Thomas403@users.noreply.github.com>
2019-10-15 09:20:50 -0500

    update

---
+++ src/Doors.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Doors {
+
+    public static void main(String[] args) {
+
+        int choice;
+        boolean text = true;
+        while (text){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Choose a door! \n 1- for Door 1 " +
+                    "\n 1- for Door 2 \n 3 - for Door 3 \n choice >>>>");
+            choice = input.nextInt();
+
+        }
+
+
+    }
+}
