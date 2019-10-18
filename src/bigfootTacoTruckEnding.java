55733c2c993ae0b0d2cd26aa40e6a8980a4e520b
Thomas403 <48694574+Thomas403@users.noreply.github.com>
2019-10-18 09:12:01 -0500

    update

--- src/bigfootTacoTruckEnding.java
+++ src/bigfootTacoTruckEnding.java
@@ -5,7 +5,7 @@ public class bigfootTacoTruckEnding {
     public static void main(String[] args) {
         int choice;
         Scanner input = new Scanner(System.in);
-        System.out.println("After you open the door you see a taco truck");
+        System.out.println("After you open the exit door you see a taco truck");
         System.out.println("What's in that taco truck is unknown so you get a choice:");
 
         System.out.println("\n 1- to look in the truck " +
@@ -13,11 +13,30 @@ public class bigfootTacoTruckEnding {
         choice = input.nextInt();
 
         if(choice == 1 ){
-            System.out.println("You choose to look in the truck after ");
+            System.out.println("You choose to look in the truck");
+            System.out.println("You then look in and see nonother than ");
 
         }
         else if(choice == 2){
             System.out.println("You chose to ignore the truck and have completed the game");
+
+            System.out.println("So I guess this means you won the game Sooo... you can leave now congrats I guess.");
+            System.out.println("                                    ");
+            System.out.println("TTTTTTTTTT  H        H    EEEEEEEEEE");
+            System.out.println("    T       H        H    E ");
+            System.out.println("    T       H        H    E ");
+            System.out.println("    T       HHHHHHHHHH    EEEEEEEEEE");
+            System.out.println("    T       H        H    E ");
+            System.out.println("    T       H        H    E ");
+            System.out.println("    T       H        H    EEEEEEEEEE");
+
+            System.out.println(" EEEEEEEEEE   N         N    D D  D ");
+            System.out.println(" E            N N       N    D     D ");
+            System.out.println(" E            N   N     N    D      D ");
+            System.out.println(" EEEEEEEEEE   N     N   N    D       D");
+            System.out.println(" E            N       N N    D      D");
+            System.out.println(" E            N        NN    D     D");
+            System.out.println(" EEEEEEEEEE   N         N    D D D ");
 
         }
 
